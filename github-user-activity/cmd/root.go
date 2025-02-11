c9945c692db4121a31a3475ab8805ae67f5c0f8a
Anthony Major <wayne.major03@gmail.com>
2025-02-11 11:42:41 -0500

    Added new go-scripts written for learning.

---
+++ github-user-activity/cmd/root.go
@@ -0,0 +1,39 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+func NewRootCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "github-user-activity",
+		Short: "Github User Activity is a CLI tool for fetching user activity",
+		Long: `Github User Activity is a CLI tool for fetching user activity. It allows you to fetch user activity by providing the username.
+
+Example:
+> github-activity fearfactor3
+
+Complete code available at https://github.com/fearfactor3/go-operation-code/github-user-activity`,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return RunDisplayActivityCmd(args)
+		},
+	}
+
+	return cmd
+}
+
+func RunDisplayActivityCmd(args []string) error {
+	if len(args) != 1 {
+		return fmt.Errorf("Please provide a username.")
+	}
+
+	username := args[0]
+	activities, err := activity.FetchGithubActivity(username)
+	if err != nil {
+		return err
+	}
+
+	return activity.DisplayActivity(username, activities)
+}
